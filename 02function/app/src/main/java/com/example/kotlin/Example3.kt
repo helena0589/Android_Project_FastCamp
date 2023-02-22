df0a20e441f3c6abf4ecf88183880406078a4ceb
조희우 <84004687+helena0589@users.noreply.github.com>
2023-02-22 09:29:45 +0900

    Feat : 03 val,var

---
+++ 02function/app/src/main/java/com/example/kotlin/Example3.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin
+
+fun main(){
+    //3. 변수
+    // val = value (값)
+    // var = variable (변경 가능한)
+    val a : Int = 3
+    var b : Int = 10
+
+    // a=20
+    b=20
+
+    //타입 추론
+    val name = "채상아"
+}
